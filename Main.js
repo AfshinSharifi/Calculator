6dbdaaba6fc6635effeb3724ac808f941d896a69
AfshinSharifi <afshinsharifi1376@gmail.com>
2022-07-26 01:01:16 +0430

    Final Calculator

---
+++ Main.js
@@ -0,0 +1,48 @@
+const display = document.querySelector('#display');
+const buttons = document.querySelectorAll('button');
+const operators = document.querySelectorAll('.operator');
+const theme = document.querySelector('#cTheme');
+disable();
+buttons.forEach(button => button.addEventListener('click', ev => {
+    const button = ev.target;
+    const buttonValue = button.innerText;
+    const currentValue = display.value;
+    if (ev.target.classList.contains('operator')) {
+        switch (buttonValue) {
+            case 'C':
+                display.value = '';
+                break;
+            case '=':
+                display.value = eval(display.value);
+                break;
+            default:
+                display.value += buttonValue;
+                disable();
+                break;
+        }
+    } else {
+        display.value += buttonValue;
+        enable();
+    }
+}));
+
+function disable() {
+    operators.forEach(operator => operator.disabled = true);
+}
+
+function enable() {
+    operators.forEach(operator => operator.disabled = false);
+}
+theme.addEventListener('click', () => {
+    const light = document.querySelector('.light');
+    const dark = document.querySelector('.dark');
+    if (theme.checked) {
+        document.querySelector('.numbers').classList.add('dark1');
+        light.style.display = "inline";
+        dark.style.display = "none";
+    } else {
+        document.querySelector('.numbers').classList.remove('dark1');
+        light.style.display = "none";
+        dark.style.display = "inline";
+    }
+});
---
+++ dist/Main.dev.js
@@ -0,0 +1,61 @@
+"use strict";
+
+var display = document.querySelector('#display');
+var buttons = document.querySelectorAll('button');
+var operators = document.querySelectorAll('.operator');
+var theme = document.querySelector('#cTheme');
+disable();
+buttons.forEach(function (button) {
+  return button.addEventListener('click', function (ev) {
+    var button = ev.target;
+    var buttonValue = button.innerText;
+    var currentValue = display.value;
+
+    if (ev.target.classList.contains('operator')) {
+      switch (buttonValue) {
+        case 'C':
+          display.value = '';
+          break;
+
+        case '=':
+          display.value = eval(display.value);
+          break;
+
+        default:
+          display.value += buttonValue;
+          disable();
+          break;
+      }
+    } else {
+      display.value += buttonValue;
+      enable();
+    }
+  });
+});
+
+function disable() {
+  operators.forEach(function (operator) {
+    return operator.disabled = true;
+  });
+}
+
+function enable() {
+  operators.forEach(function (operator) {
+    return operator.disabled = false;
+  });
+}
+
+theme.addEventListener('click', function () {
+  var light = document.querySelector('.light');
+  var dark = document.querySelector('.dark');
+
+  if (theme.checked) {
+    document.querySelector('.numbers').classList.add('dark1');
+    light.style.display = "inline";
+    dark.style.display = "none";
+  } else {
+    document.querySelector('.numbers').classList.remove('dark1');
+    light.style.display = "none";
+    dark.style.display = "inline";
+  }
+});
